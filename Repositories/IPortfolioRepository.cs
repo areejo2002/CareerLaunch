9dd5612a9c1dfef2061f81879df4ea86fc8a5681
areejo2002 <153197642+areejo2002@users.noreply.github.com>
2025-06-20 17:25:36 +0300

    Add files via upload

---
+++ Repositories/IPortfolioRepository.cs
@@ -0,0 +1,19 @@
+﻿using CareerLaunchpad.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CareerLaunchpad.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+        Task CreateAsync(Portfolio portfolio);
+        Task<List<Portfolio>> GetAllForUserAsync(string endUserId);
+        Task<Portfolio?> GetByIdAsync(int id);
+        Task<Portfolio?> GetPortfolioWithDetailsAsync(int id);
+
+        Task UpdateAsync(Portfolio portfolio);
+        Task DeleteAsync(int id);
+        Task<List<Portfolio>> GetAllAsync();
+
+    }
+}
